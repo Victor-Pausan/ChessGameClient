2ad98fb86080e3f188b0ba8e5bb080d518d6b598
Victor-Pausan <pausanvictor@gmail.com>
2023-12-06 10:56:59 +0200

    Add project files.

---
+++ fClient.Designer.cs
@@ -0,0 +1,174 @@
+﻿
+namespace ChessGame
+{
+    partial class fClient
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.tbAddress = new System.Windows.Forms.TextBox();
+            this.btnConect = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.tbDate = new System.Windows.Forms.TextBox();
+            this.btnSend = new System.Windows.Forms.Button();
+            this.btnExit = new System.Windows.Forms.Button();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(186, 581);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(80, 17);
+            this.label1.TabIndex = 11;
+            this.label1.Text = "IP Address:";
+            // 
+            // tbAddress
+            // 
+            this.tbAddress.Location = new System.Drawing.Point(274, 578);
+            this.tbAddress.Margin = new System.Windows.Forms.Padding(4);
+            this.tbAddress.Name = "tbAddress";
+            this.tbAddress.Size = new System.Drawing.Size(182, 22);
+            this.tbAddress.TabIndex = 10;
+            this.tbAddress.Text = "127.0.0.1";
+            // 
+            // btnConect
+            // 
+            this.btnConect.Location = new System.Drawing.Point(189, 444);
+            this.btnConect.Margin = new System.Windows.Forms.Padding(4);
+            this.btnConect.Name = "btnConect";
+            this.btnConect.Size = new System.Drawing.Size(267, 46);
+            this.btnConect.TabIndex = 9;
+            this.btnConect.Text = "Start";
+            this.btnConect.UseVisualStyleBackColor = true;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Enabled = false;
+            this.textBox1.Location = new System.Drawing.Point(13, 13);
+            this.textBox1.Margin = new System.Windows.Forms.Padding(4);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.textBox1.Size = new System.Drawing.Size(25, 24);
+            this.textBox1.TabIndex = 8;
+            // 
+            // tbDate
+            // 
+            this.tbDate.Enabled = false;
+            this.tbDate.Location = new System.Drawing.Point(46, 15);
+            this.tbDate.Margin = new System.Windows.Forms.Padding(4);
+            this.tbDate.Name = "tbDate";
+            this.tbDate.Size = new System.Drawing.Size(36, 22);
+            this.tbDate.TabIndex = 6;
+            // 
+            // btnSend
+            // 
+            this.btnSend.Enabled = false;
+            this.btnSend.Location = new System.Drawing.Point(13, 45);
+            this.btnSend.Margin = new System.Windows.Forms.Padding(4);
+            this.btnSend.Name = "btnSend";
+            this.btnSend.Size = new System.Drawing.Size(36, 25);
+            this.btnSend.TabIndex = 7;
+            this.btnSend.Text = "Send";
+            this.btnSend.UseVisualStyleBackColor = true;
+            // 
+            // btnExit
+            // 
+            this.btnExit.Location = new System.Drawing.Point(189, 512);
+            this.btnExit.Name = "btnExit";
+            this.btnExit.Size = new System.Drawing.Size(267, 46);
+            this.btnExit.TabIndex = 15;
+            this.btnExit.Text = "Exit";
+            this.btnExit.UseVisualStyleBackColor = true;
+            this.btnExit.Click += new System.EventHandler(this.btnExit_Click);
+            // 
+            // textBox2
+            // 
+            this.textBox2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(100)))), ((int)(((byte)(152)))), ((int)(((byte)(51)))));
+            this.textBox2.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.textBox2.Font = new System.Drawing.Font("Vladimir Script", 40F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox2.Location = new System.Drawing.Point(158, 45);
+            this.textBox2.Multiline = true;
+            this.textBox2.Name = "textBox2";
+            this.textBox2.ReadOnly = true;
+            this.textBox2.Size = new System.Drawing.Size(337, 99);
+            this.textBox2.TabIndex = 13;
+            this.textBox2.Text = "Chess Game";
+            this.textBox2.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::ChessGame.Properties.Resources.white_knight;
+            this.pictureBox1.Location = new System.Drawing.Point(189, 150);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(267, 266);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 12;
+            this.pictureBox1.TabStop = false;
+            // 
+            // fClient
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(100)))), ((int)(((byte)(152)))), ((int)(((byte)(51)))));
+            this.ClientSize = new System.Drawing.Size(641, 616);
+            this.Controls.Add(this.btnExit);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.tbAddress);
+            this.Controls.Add(this.btnConect);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.btnSend);
+            this.Controls.Add(this.tbDate);
+            this.Name = "fClient";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "fClient";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox tbAddress;
+        private System.Windows.Forms.Button btnConect;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox tbDate;
+        private System.Windows.Forms.Button btnSend;
+        private System.Windows.Forms.Button btnExit;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.PictureBox pictureBox1;
+    }
+}
---
+++ fClient.cs
@@ -0,0 +1,171 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ChessGame
+{
+    public partial class fClient : Form
+    {
+        Form1 mainForm;
+
+        public TcpClient client;
+        public NetworkStream clientStream;
+        public bool ascult;
+        public fClient clientForm;
+        public Thread t;
+
+        public fClient(Form1 mainForm)
+        {
+            InitializeComponent();
+
+            textBox1.Hide();
+            tbDate.Hide();
+            btnSend.Hide();
+
+            this.mainForm = mainForm;
+            clientForm = this;
+            btnConect.Click += btnConect_Click;
+            tbDate.KeyPress += tbDate_KeyPress;
+            btnSend.Click += btnSend_Click;
+            this.FormClosed += FServer_FormClosed;
+        }
+
+        private void Asculta_client()
+        {
+            StreamReader citire = new StreamReader(clientStream);
+            String dateClient;
+            while (ascult)
+            {
+                dateClient = citire.ReadLine();
+
+                MethodInvoker m = new MethodInvoker(() => clientForm.textBox1.Text += ("Server: " + dateClient + Environment.NewLine)) ;
+                clientForm.textBox1.Invoke(m);
+
+                MethodInvoker n = new MethodInvoker(() => mainForm.game.UpdateTable(Convert.ToInt32(dateClient[0] - '0'),
+                            Convert.ToInt32(dateClient[1] - '0'),
+                            Convert.ToInt32(dateClient[2] - '0'),
+                            Convert.ToInt32(dateClient[3]) - '0'));
+                mainForm.Invoke(n);
+            }
+        }
+
+        private void btnSend_Click(object sender, EventArgs e)
+        {
+            try
+            {
+
+                StreamWriter scriere = new StreamWriter(clientStream);
+                scriere.AutoFlush = true; // enable automatic flushing
+                scriere.WriteLine(tbDate.Text);
+                textBox1.Text += "Client: " + tbDate.Text + Environment.NewLine;
+                tbDate.Clear();
+                // s_text.Close();
+            }
+            finally
+            {
+                // code in finally block is guranteed 
+                // to execute irrespective of 
+                // whether any exception occurs or does 
+                // not occur in the try block
+                //  client.Close();
+            }
+        }
+
+        private void FServer_FormClosed(object sender, FormClosedEventArgs e)
+        {
+            ascult = false;
+            t.Abort();
+            clientStream.Close();
+            client.Close();
+        }
+
+        private void tbDate_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (e.KeyChar == Convert.ToChar(Keys.Enter))
+            {
+                btnSend_Click(sender, e);
+            }
+        }
+
+        private void btnConect_Click(object sender, EventArgs e)
+        {
+
+            if (btnConect.Text == "Start")
+            {
+                if (tbAddress.Text.Length > 0)
+                {
+                    try
+                    {
+                        client = new TcpClient(tbAddress.Text, 3000);
+                        this.WindowState = System.Windows.Forms.FormWindowState.Minimized;
+                        mainForm.WindowState = System.Windows.Forms.FormWindowState.Normal;
+                    }
+                    catch(Exception exp)
+                    {
+                        textBox2.Text = "Waiting for server...";
+                    }
+                    
+                    ascult = true;
+                    t = new Thread(new ThreadStart(Asculta_client));
+                    t.Start();
+                    clientStream = client.GetStream();
+                    tbDate.Enabled = true;
+                    btnSend.Enabled = true;
+                    label1.Visible = false;
+                    tbAddress.Visible = false;
+                    btnConect.Text = "Disconect";
+                }
+                else
+                {
+                    MessageBox.Show("Specificati adresa de IP");
+                }
+            }
+            else
+            {
+                ascult = false;
+                t.Abort();
+                StreamWriter scriere = new StreamWriter(clientStream);
+                scriere.AutoFlush = true; // enable automatic flushing
+                scriere.WriteLine("#Gata");
+            }
+
+        }
+
+        public void Set_tbDate(string pozitii)
+        {
+            tbDate.Text = pozitii;
+            try
+            {
+
+                StreamWriter scriere = new StreamWriter(clientStream);
+                scriere.AutoFlush = true; // enable automatic flushing
+                scriere.WriteLine(tbDate.Text);
+                textBox1.Text += "Client: " + tbDate.Text + Environment.NewLine;
+                tbDate.Clear();
+                // s_text.Close();
+            }
+            finally
+            {
+                // code in finally block is guranteed 
+                // to execute irrespective of 
+                // whether any exception occurs or does 
+                // not occur in the try block
+                //  client.Close();
+            }
+        }
+
+        private void btnExit_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+    }
+}
